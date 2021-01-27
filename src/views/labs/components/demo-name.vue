2071ac2402ae4c8f3699d83b2437510ef4232752
sufangyu <sufangru@baibu.la>
2021-01-28 00:12:29 +0800

    feat: 增加 请求示例

--- src/views/labs/components/demo-name.vue
+++ src/views/labs/components/demo-name.vue
@@ -1,8 +1,12 @@
 <template>
-  <section class="demo-name">
-    <router-link :to="{ path: '/labs'}">
-      实验室首页
+  <section class="demo-title">
+    <router-link
+      class="icon"
+      :to="{ path: '/labs'}"
+    >
+      <van-icon name="wap-home-o" />
     </router-link>
+    {{ title }}
   </section>
 </template>
 
@@ -10,12 +14,44 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
   setup() {
     return {};
   },
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.demo-title {
+  vertical-align: middle;
+  font-size: 18px;
+  padding: 20px 0 20px 15px;
+  text-align: left;
+  height: 20px;
+  line-height: 20px;
+  box-sizing: content-box;
 
+  .icon {
+    margin-right: 20px;
+    font-size: 20px;
+    position: relative;
+    vertical-align: middle;
+
+    &::after {
+      content: "";
+      position: absolute;
+      top: 1px;
+      right: -14px;
+      width: 1px;
+      height: 15px;
+      background-color: #ccc;
+    }
+  }
+
+}
 </style>
--- src/views/labs/request.vue
+++ src/views/labs/request.vue
@@ -1,11 +1,16 @@
 <template>
   <section>
-    <demo-name name="Request 请求"></demo-name>
+    <demo-name :title="pageTitle"></demo-name>
+    <div class="demo-content">
+      <van-button type="primary" @click="handleGetList">GET 请求</van-button>
+      <van-button type="success" @click="handlePost">POST 请求</van-button>
+    </div>
   </section>
 </template>
 
 <script>
 import { defineComponent, getCurrentInstance } from 'vue';
+import { getList, createNews } from '@/api/test';
 import DemoName from './components/demo-name.vue';
 
 export default defineComponent({
@@ -13,14 +18,46 @@ export default defineComponent({
     DemoName,
   },
   setup() {
-    const currentPage = getCurrentInstance();
-    console.log(currentPage);
+    const { currentRoute } = getCurrentInstance().ctx.$router;
+    const meta = currentRoute?.value?.meta ?? {};
+
+    async function handleGetList() {
+      const params = {
+        ids: [1, 2, 3, 4],
+      };
+      try {
+        const { data } = await getList(params);
+        console.log(data?.list);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    async function handlePost() {
+      const { data } = await createNews({
+        title: '标题',
+        content: '内容',
+        name: '张三疯',
+      });
+      console.log(data);
+    }
 
     return {
+      pageTitle: meta?.title,
+      handleGetList,
+      handlePost,
     };
   },
 });
 </script>
 
 <style lang="scss" scoped>
+.demo-content {
+  padding: 15px;
+
+  .van-button--small,
+  .van-button--normal:not(:last-child) {
+    margin-right: 16px;
+  }
+}
 </style>
