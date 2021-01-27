58b9e3ec752614758192b12c961606a21a62840d
sufangyu <sufangru@baibu.la>
2021-01-28 01:42:13 +0800

    feat: 增加 两翼留白全局组件

---
+++ src/components/wing-blank/wing-blank.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="wingblank"
+    :class="sizeClass"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+
+// 两翼留白的间距，可选sm, md, lg
+export default defineComponent({
+  name: 'WingBlank',
+  props: {
+    size: {
+      type: String,
+      default: 'lg',
+      validator: (value: string) => ['sm', 'md', 'lg'].indexOf(value) !== -1,
+    },
+  },
+  setup(props) {
+    return {
+      sizeClass: computed(() => `wingblank-${props.size}`),
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.wingblank {
+  &.wingblank-sm {
+    margin-left: 5px;
+    margin-right: 5px;
+  }
+  &.wingblank-md {
+    margin-left: 8px;
+    margin-right: 8px;
+  }
+  &.wingblank-lg {
+    margin-left: 15px;
+    margin-right: 15px;
+  }
+}
+</style>
+
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import { createApp } from 'vue';
 import Vant from 'vant';
 import 'vant/lib/index.css';
 
+import WingBblank from './components/wing-blank';
+
 import App from './App.vue';
 import router from './router';
 import store, { key } from './store';
 import './permission';
 
-createApp(App)
-  .use(store, key)
+const app = createApp(App);
+app.use(store, key)
   .use(router)
   .use(Vant)
   .mount('#app');
+
+// 全局组件
+app.component('wing-blank', WingBblank);
